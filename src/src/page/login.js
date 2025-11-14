719ea60fdfdbffbd1b494e77abb21ae523b96f7b
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-14 05:43:31 -0800

    Create login.js

---
+++ src/src/page/login.js
@@ -0,0 +1,11 @@
+// src/pages/Login.js
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <p>Please enter your credentials to access your account.</p>
+    </div>
+  );
+}
