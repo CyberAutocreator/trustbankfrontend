2a67dba766604dec7bcbcfbd5046ce41e4ae7147
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-14 15:17:27 +0100

    Create home.js

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>TrustBank Frontend</h1>
+      <p>Welcome to the TrustBank frontend. This is the Home page.</p>
+    </div>
+  );
+}
