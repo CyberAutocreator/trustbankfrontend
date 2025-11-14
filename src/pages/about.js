c784a30f3a235e566c27460f18b70a6d038129d1
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-14 15:18:32 +0100

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About TrustBank</h1>
+      <p>Information about the TrustBank frontend.</p>
+    </div>
+  );
+}
