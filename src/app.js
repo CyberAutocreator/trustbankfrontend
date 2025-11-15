cf3380e04f7fc48b27196038f4838c64acbcd0dd
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-15 03:41:13 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import NotFound from './pages/NotFound';
+import Layout from './layout/Layout';
 
-function App() {
+export default function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL || '/'}>
+    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
+    </Layout>
   );
 }
-
-export default App;
