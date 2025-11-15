a09480ac4c01a3da9bd586b3d99e904dfa15f17b
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-15 07:09:39 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Layout from './layout/Layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 
-export default function App() {
+function App() {
   return (
-    <Layout>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Layout>
+    </BrowserRouter>
   );
 }
+
+export default App;
