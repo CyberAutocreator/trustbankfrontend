1d655f367a7050597c2b09078e057e8a15bf89fc
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-24 22:48:18 +0100

    Update babelrc.js

--- babelrc.js
+++ babelrc.js
@@ -1,20 +1,7 @@
-module.exports = {
-presets: [
-[
-"@babel/preset-env",
 {
-targets: {
-browsers: [">0.25%", "not dead"]
-},
-useBuiltIns: "usage",
-corejs: 3
+  "plugins": [
+    "@babel/plugin-transform-class-properties",
+    "@babel/plugin-transform-optional-chaining",
+    "@babel/plugin-transform-private-methods"
+  ]
 }
-]
-],
-plugins: [
-"@babel/plugin-transform-nullish-coalescing-operator",
-"@babel/plugin-transform-numeric-separator",
-"@babel/plugin-transform-class-properties",
-"@babel/plugin-transform-private-property-in-object"
-]
-};
