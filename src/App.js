fcface014697f57d97a7a4866b1287d1a6eb33dc
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-25 19:55:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <Navbar />
+      <Routes>{/* your routes */}</Routes>
     </Router>
   );
 }
-
-export default App;
