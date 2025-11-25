19a2a9d1fdaa6c19e17350418de6ed10bd48d423
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-25 19:23:27 +0100

    Update dashboard.js

--- src/src/page/dashboard.js
+++ src/src/page/dashboard.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
   const [message, setMessage] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem('token');
+    if (!token) return navigate('/');
 
     fetch(`${import.meta.env.VITE_API_URL}/dashboard`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
+      headers: { Authorization: `Bearer ${token}` }
     })
       .then(res => res.json())
       .then(data => setMessage(data.message))
-      .catch(err => console.error('Error:', err));
+      .catch(err => console.error(err));
   }, []);
 
   return <h1>{message}</h1>;
