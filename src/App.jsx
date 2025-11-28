c1aeadc8a5b07878b6e189f12b11c4cd356822df
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-28 20:31:57 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to TrustBank Frontend 🚀</h1>
+      <p>Your app is running successfully with Vite + React.</p>
+    </div>
+  );
+}
+
+export default App;
