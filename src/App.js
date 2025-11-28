608aacf5e0eb8c2078d6df30be070416927f60d6
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-28 05:30:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Dashboard from "./components/Dashboard";
+import "./App.css";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -9,10 +10,10 @@ function App() {
   return (
     <div>
       {!user ? (
-        <>
+        <div className="auth-container">
           <Login onLogin={setUser} />
           <Signup onSignup={setUser} />
-        </>
+        </div>
       ) : (
         <Dashboard user={user} />
       )}
