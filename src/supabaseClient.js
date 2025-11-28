e1e9fe9be93364af394bed522df7628623ee19a1
CyberAutocreator <cryptoautorecovery@gmail.com>
2025-11-28 04:01:37 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Use environment variables from Vercel
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
