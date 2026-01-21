b55236ceb0c5744f969d28f79b105a8ab04fe194
CyberAutocreator <cryptoautorecovery@gmail.com>
2026-01-21 22:53:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
-// server.js: Simple Express server to serve the React/Front-end build directory
-
-const express = require('express');
-const path = require('path');
+const express = require("express");
 const app = express();
 
-// Serve the static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Handle requests that aren't for static files by serving the main index.html
-// This allows front-end routing (like React Router) to work correctly.
-app.get('*', (req, res) => {
-  // We assume the built files are in a directory named 'build'
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel + Express!");
 });
 
-// Use the PORT provided by the hosting environment, or default to 3000
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running and serving files from the 'build' folder on port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
